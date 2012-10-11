f5cb8d10593ba9bfdc0c87668d3f8cf47c942d16
sebastien.zurfluh <sebastien.zurfluh@gmail.com>
2012-10-11 12:49:42 +0200

    Setting up new repo for CMS module.

---
+++ src/ch/sebastienzurfluh/client/SwissMuseum___Mimosa3.java
@@ -0,0 +1,15 @@
+package ch.sebastienzurfluh.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class SwissMuseum___Mimosa3 implements EntryPoint {
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		
+	}
+}
